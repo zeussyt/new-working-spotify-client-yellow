6e7d40f41e73c8b91fb0d452f338dcdc6a404563
Noah Cross <theorigamiman55@gmail.com>
2026-05-02 00:39:35 -0400

    small fix fr

--- src/components/PlayerBar.jsx
+++ src/components/PlayerBar.jsx
@@ -13,12 +13,3 @@ export default function PlayerBar({ track, isPlaying, onPlay, onPause }) {
         </div>
     );
 }
-    if (!track) return null;
-
-    return (
-        <div>
-            <img src={track.album.images[0].url} />
-            <p>{track.name}</p>
-            <p>{track.artists[0].name}</p>
-        </div>
-    );
